a3e44c6a57858a14448b880e70ff81b198a9b963
Olasunkanmi Fafowora <sunkanmifafowora@gmail.com>
2021-04-25 17:25:27 +0100

    done home page

--- signin.php
+++ signin.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 
 <html>
@@ -10,36 +9,38 @@
     <title>Sign in</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link href = "signin.css" rel = "stylesheet"  type = "text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="./css/signin.css" rel="stylesheet" type="text/css">
 </head>
+
 <body>
     <div class="form">
         <div class="tab-header">
-    <h1>Profile</h1>
-    <a  href = ./sign.html > Signin</a> </pre><a href = ./Registration.html>Registration</a>
-<br>   
-   
-<div class = form-element >
-<input type = "text1" class= "input-field"  placeholder= "Email"><br>
+            <h1>Profile</h1>
+            <a href=./sign.html> Signin</a> </pre><a href=./Registration.html>Registration </a> <br>
 
-</div>
+                <div class=form-element>
+                    <input type="text1" class="input-field" placeholder="Email"><br>
 
-<div class = form-element >
+                </div>
+
+                <div class=form-element>
 
 
-<input type = "text1" class= "input-field"  placeholder= "Password"><br>
-</div>
+                    <input type="text1" class="input-field" placeholder="Password"><br>
+                </div>
 
-<div class="form-element">
+                <div class="form-element">
 
-<input type="button1" value="Sign in"><br>
+                    <input type="button1" value="Sign in"><br>
 
-</div>
+                </div>
 
-<a  href = ./Registration.html > Registration </a>
-<br>
-        
+                <a href=./Registration.html> Registration </a>
+                <br>
+
 
 </body>
+
 </html>
