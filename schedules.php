e8c791f17cc452a9215c7bf45886bcebc8b8cace
Olasunkanmi Fafowora <sunkanmifafowora@gmail.com>
2021-05-01 12:00:40 +0100

    done with sidebar compression

--- schedules.php
+++ schedules.php
@@ -35,60 +35,9 @@
 
 <body>
   <main>
-    <section class="header-sidebar">
-      <input type="checkbox" id="toggle-butt" />
-
-      <label for="toggle-butt" class="toggle-menu">
-        <i class="fa fa-bars" aria-hidden="true"></i>
-      </label>
-
-      <section class="side-bar-section">
-        <div class="profile-image">
-          <img src="./imgs/demo.png" alt="profile" />
-          <hr />
-        </div>
-
-        <div class="links-to-pages">
-          <div class="page-link">
-            <a href="./index.php">
-              <img src="./imgs/appointment.svg" alt="">
-              <p>Appointments</p>
-            </a>
-          </div>
-          <div class="page-link">
-            <a href="./schedules.php">
-              <img src="./imgs/schedules.svg" alt="">
-              <p>Schedules</p>
-            </a>
-          </div>
-          <div class="page-link">
-            <a href="/">
-              <img src="./imgs/sessions.svg" alt="">
-              <p>Sessions</p>
-            </a>
-          </div>
-          <div class="page-link">
-            <a href="/">
-              <img src="./imgs/mail.svg" alt="">
-              <p>Message</p>
-            </a>
-          </div>
-          <div class="page-link">
-            <a href="/">
-              <img src="./imgs/admin.svg" alt="">
-              <p>Admin</p>
-            </a>
-          </div>
-          <div class="page-link">
-            <a href="/">
-              <img src="./imgs/logout.svg" alt="">
-              <p>Logout</p>
-            </a>
-          </div>
-        </div>
-      </section>
-    </section>
-
+    <?php
+        include "./sidebar.php";
+    ?>
     <section class="p415-main-section">
       <h1 id="p415-schedule-head">Schedules</h1>
       <hr />
