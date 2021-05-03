b901b3a36d549c435b8b2e091241678869a0bfa0
Olasunkanmi Fafowora <sunkanmifafowora@gmail.com>
2021-05-03 14:07:17 +0100

    added border radius

---
+++ js/dashboard.js
@@ -0,0 +1,11 @@
+const allStatuses = document.querySelectorAll(".p415-work-status-update-green");
+
+for (let i = 0; i < allStatuses.length; i++) {
+    if (allStatuses[i].innerText === "Cancelled") {
+        allStatuses[i].classList.remove("p415-work-status-update-green");
+        allStatuses[i].classList.add("p415-work-status-update-red");
+    } else if (allStatuses[i].innerText === "Pending") {
+        allStatuses[i].classList.remove("p415-work-status-update-green");
+        allStatuses[i].classList.add("p415-work-status-update-pink");
+    }
+}
