b6e6a01c9f0fd097de06b0cc0f96ceb718cf43c8
Olasunkanmi Fafowora <sunkanmifafowora@gmail.com>
2021-05-05 01:24:05 +0100

    doings

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ if(session_status() == PHP_SESSION_NONE){
 
 if ( !isset($_SESSION['user-matric'])) {
     header("Location: login.php");
-
 }
 
 include 'connect.php';
@@ -31,6 +30,33 @@ if ($result = mysqli_query($conn,$usr)) {
     $profilepic = $usr_row['student_img_url'];
     
   //  echo $profilepic;
+}
+
+$select_items = "";
+
+$query1 = "SELECT * FROM `dbnh41dWFL`.`session`
+            WHERE status= 'active' ";
+
+if ($resullt = mysqli_query($conn,$query1)) {
+
+    $active_session = mysqli_fetch_array($resullt);
+
+//    echo $active_session['session'];
+   // while ( $row = mysqli_fetch_array($resullt)) {
+        
+  //      echo $row['session'];
+ //   }  
+
+ $query2 = "SELECT * FROM `dbnh41dWFL`.`schedule`
+ WHERE session= '".$active_session['session']."' ";
+
+if ($resullt2 = mysqli_query($conn,$query2)) {
+        while ( $row2 = mysqli_fetch_array($resullt2)) {       
+            
+            $select_items = $row2['available_DOW'].' '.date('F, Y', strtotime($row2['start_date'])).' '.date('h:ia', strtotime($row2['start_time'])).'-'.date('h:ia', strtotime($row2['end_time']));
+        }  
+    }
+
 }
 
 //$dte = "";
@@ -59,8 +85,8 @@ if ($result = mysqli_query($conn,$usr)) {
         <nav>
             <ul>
                 <li class="active"><a href="./">home</a></li>
-                <li><a href="./">profile</a></li>
-                <li><a href="./">logout</a></li>
+                <li><a href="./profile.php">profile</a></li>
+                <li><a href="./logout.php">logout</a></li>
                 <li><a href="./"><img src=<?php echo $profilepic ?> alt="profile"></a></li>
             </ul>
         </nav>
@@ -78,47 +104,15 @@ if ($result = mysqli_query($conn,$usr)) {
             <img src="./imgs/home-background.png" alt="" />
         </section>
 
-        <section class="p415-bottom-section">
-            <div class="dropdown">
-                <button class="dropbtn" onclick="showDropDown()"> Available Dates
+        <form method="GET" class="p415-bottom-section">
+        <input type="text" disabled value="" name="selected-date">
+            <div type="text" class="dropdown">
+                <span class="dropbtn" onclick="showDropDown()"> Available Dates
                     <i class="fa fa-caret-down"></i>
-                </button>
+                </span>
                 <div class="dropdown-content" id="myDropdown">
-                    <?php 
-
-                    $select_items = "";
-                    
-                    $query1 = "SELECT * FROM `dbnh41dWFL`.`session`
-                                WHERE status= 'active' ";
-
-                    if ($resullt = mysqli_query($conn,$query1)) {
-                    
-                        $active_session = mysqli_fetch_array($resullt);
-
-                    //    echo $active_session['session'];
-                       // while ( $row = mysqli_fetch_array($resullt)) {
-                            
-                      //      echo $row['session'];
-                     //   }  
-
-                     $query2 = "SELECT * FROM `dbnh41dWFL`.`schedule`
-                     WHERE session= '".$active_session['session']."' ";
-                    
-                    if ($resullt2 = mysqli_query($conn,$query2)) {
-
-                            while ( $row2 = mysqli_fetch_array($resullt2)) {       
-                                $select_items = $row2['available_DOW'].' '.date('F, Y', strtotime($row2['start_date'])).' '.date('h:ia', strtotime($row2['start_time'])).'-'.date('h:ia', strtotime($row2['end_time']));
-                                echo   '<a href="#selected_time">'.$select_items.'</a>' ;
-                            }  
-                            
-                
-                        }
-
-                    }
-
-                    
-                    ?>
-
+                      <?php echo '<span>'.$select_items.'</span>';
+                      ?>
                 </div>
             </div>
             <div class="p415-slot-section">
@@ -131,11 +125,10 @@ if ($result = mysqli_query($conn,$usr)) {
             <time id="selected_time" datetime="2021-03-22T17:30">Monday. 22nd March 2021 5:30pm</time>
 
             <label for="leave-a-message">Leave a message</label>
-            <textarea cols="5" rows="8" id="leave-a-message" placeholder="Leave a message..."
-                name="booking-message"></textarea>
+            <textarea cols="5" rows="8" id="leave-a-message" placeholder="Leave a message..." name="booking-message"></textarea>
 
             <input type="submit" value="BOOK" />
-        </section>
+        </form>
 
     </main>
 
---
+++ logout.php
@@ -0,0 +1,5 @@
+<?php
+    session_start();
+    unset($_SESSION["user-matric"])
+    header("Location:login.php");
+?>
